51acad82221f6f9bb33ed30033ab2551e1fc9a11
jhtr0226 <johanncraft17@gmail.com>
2023-09-15 21:55:59 -0600

    Prep 1 and 2 ready

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {char.ToUpper(lastName[0])}{lastName.Substring(1)}, {char.ToUpper(name[0])}{name.Substring(1)} {char.ToUpper(lastName[0])}{lastName.Substring(1)}.");
     }
 }
+
+
+//goal
+// bond james bond
+
+//How i understood it
+// Console.Write/read = everything {add line to pass make it print}
+// then name the variable
+// "string "name" = console.read"
+// input = console.write
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,112 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        // ///////////////////////IF block/////////////////////////
+        string letter = "";
+
+        if (number >= 90)
+        {
+            if (number >= 93)
+                letter = "A";
+            else
+            {
+                letter = "A-";
+            }
+        }
+
+
+        else if (number >= 80)
+        {
+            if (number >= 87 && number <= 89)
+            {
+                letter = "B+";
+            }
+            else if (number >= 83 && number <= 86)
+            {
+                letter = "B";
+            }
+            else if (number >= 80 && number <= 82)
+            {
+                letter = "B-";
+            }
+
+        }
+
+
+        else if (number >= 70)
+        {
+            if (number >= 77 && number <= 79)
+            {
+                letter = "C+";
+            }
+            else if (number >= 73 && number <= 76)
+            {
+                letter = "C";
+            }
+            else if (number >= 70 && number <= 72)
+            {
+                letter = "C-";
+            }
+
+        }
+        else if (number >= 60)
+        {
+            if (number >= 67 && number <= 69)
+            {
+                letter = "D+";
+            }
+            else if (number >= 65 && number <= 66)
+            {
+                letter = "D";
+            }
+            else if (number >= 60 && number <= 64)
+            {
+                letter = "D-";
+            }
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+        // //////////////////////Adding an "n"///////////////////////////
+
+        if (letter == "A" || letter == "A-")
+        {
+            Console.WriteLine($"Awesome!\nYour final grade is an {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Your final grade is a {letter}");
+        }
+        // ////////////////////////////Final message////////////////////////
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!\nRemember always to give your best!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! You better study next time!\nAnd remember to believe in yourself!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
