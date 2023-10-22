3d82be4c2e492e45ce0ebcd35830e531f1279df9
jhtr0226 <johanncraft17@gmail.com>
2023-10-21 21:17:27 -0600

    week 06

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.IO;
+using System.Data;
+using System.Runtime.InteropServices.Marshalling; //i don't know what this is and I didn't put it there, maybe it was automatic from one of the deleted lines?
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        List<Scripture> scriptures = new List<Scripture>();
+        List<int> alreadyHidden = new List<int>();
+
+
+
+        string[] lines = File.ReadAllLines("Books.txt");//reader
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+
+            string book = parts[0];
+            string reference = parts[1];
+            string text = parts[2];
+
+            //getting chapter,verses from the reference
+            string[] referenceParts = reference.Split(':');
+            int chapter = int.Parse(referenceParts[0]);
+
+            string[] verseParts = referenceParts[1].Split('-');
+            int verseStart = int.Parse(verseParts[0]);
+            int verseEnd = verseParts.Length > 1 ? int.Parse(verseParts[1]) : verseStart;
+
+            //I learned that ? is like a condition (quick if) for a line, comes along with : for else lol
+
+            List<string> verseWords = new List<string>(text.Split(' '));//separates the words
+
+            Reference referenceObj = new Reference(book, chapter, verseStart, verseEnd);
+            Scripture scripture = new Scripture(referenceObj, verseWords);
+            //new scripture object
+
+            scriptures.Add(scripture); //adds scripture to the list of scriptures
+        }
+
+        Random random = new Random();
+        Scripture currentScripture = scriptures[random.Next(0, scriptures.Count)];
+
+        while (!currentScripture.AllWordsHidden()) //loops until words get to be all hidden
+        {
+            Console.Clear();
+            Console.WriteLine(currentScripture.getRenderedText());
+            Console.Write("\nPress enter to continue or type quit to finish:\n");
+            string importantChoice = Console.ReadLine();
+
+            if (importantChoice == "")
+            {
+                currentScripture.hiding(alreadyHidden); //calls to hide words form the selected scripture
+
+                if (currentScripture.AllWordsHidden())
+                {
+                    Console.Clear();
+                    Console.WriteLine(currentScripture.getRenderedText());
+                    /*fun fact, the last 2 lines of code took me around 4 hours to put them here
+                    I kept reaching the end without getting everything hidden, I thought the problems was in 
+                    the hiding method but it was this break from below*/
+                    break;
+                }
+            }
+            else if (importantChoice.ToLower() == "quit")
+            {
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        Console.WriteLine("Until next time!");
     }
 }
---
+++ prove/Develop03/Scriptures.cs
@@ -0,0 +1,100 @@
+using System.Text;
+
+public class Scripture
+{
+
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private List<int> _hidingIndices = new List<int>(); //tracks hidden words
+    private Random _random = new Random();
+
+    //setter
+    public Scripture(Reference reference, List<string> verseWords)
+    {
+        _reference = reference;
+
+        foreach (string wordText in verseWords)
+        {
+            _words.Add(new Word(wordText)); //adds to word's list
+        }
+    }
+
+
+    //methods
+
+
+    public void HideLastThreeWords()
+    {
+        Random random = new Random();
+        //loops 3 times
+        for (int indexNumber = 0; indexNumber < 3; indexNumber++)
+        {
+            int randomIndexNumber;
+            do
+            {//gets a random index
+                randomIndexNumber = random.Next(0, _words.Count);
+            } while (_hidingIndices.Contains(randomIndexNumber)); //trying not to repeat it
+
+            _hidingIndices.Add(randomIndexNumber);//added to hidingIndices list
+            _words[randomIndexNumber].Hide();//hiding the words
+        }
+    }
+    //Hides some words
+    public void hiding(List<int> alreadyHiddenIndices)
+    {
+        List<int> availableIndices = _words
+            .Select((word, index) => new { Word = word, Index = index }) //selects a new object
+            .Where(item => !item.Word.IsHidden() && !alreadyHiddenIndices.Contains(item.Index))
+            //filters non hidden words from hidden ones (done this by research (took me like 3 days) and found out it loks like javascript)
+            .Select(item => item.Index) //selects indices
+            .ToList(); //converts to a list
+
+
+        /*that last part was a miracle, a friend told me to uses those .select, .where and .ToList
+        that blew my mind, I should study them more*/
+
+        int hideCount = Math.Min(3, availableIndices.Count); //words to hide
+
+        for (int counter = 0; counter < hideCount; counter++) //loop to hide words
+        {
+            int randomIndex = _random.Next(availableIndices.Count); //generates a random index
+            _words[availableIndices[randomIndex]].Hide(); //hides our lucky generated index
+            alreadyHiddenIndices.Add(availableIndices[randomIndex]); //we add it to the "hidden" list
+            availableIndices.RemoveAt(randomIndex);// and make sure not to repeat that index by removing it form the rest
+        }
+    }
+
+
+
+    public bool AllWordsHidden()
+    {
+        return _words.TrueForAll(word => word.IsHidden()); //makes sure everything is hidden
+    }
+
+    public string getRenderedText()
+    {
+
+        //used to efficiently concatenate strings in situations where you're doing a lot of string manipulations.
+        //I must study it more
+        StringBuilder renderedText = new StringBuilder();
+
+        renderedText.Append($"{_reference.gettingBook()} {_reference.gettingChapter()}:{_reference.gettingInitialVerse()}-{_reference.gettingFinalVerse()} ");
+
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden())
+            {
+                renderedText.Append(new string('_', word.GetText().Length) + " ");
+                //if hidden, display it with _
+            }
+            else
+            {
+                renderedText.Append($"{word.GetText()} ");
+                //if not, use the word
+            }
+        }
+
+        return renderedText.ToString().Trim();
+        //// Convert StringBuilder to string and trim trailing whitespace
+    }
+}
